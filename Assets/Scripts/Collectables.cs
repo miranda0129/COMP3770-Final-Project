802ccc25c4701466f8990f9b3ef648309ab42b16
miranda0129 <56649574+miranda0129@users.noreply.github.com>
2020-12-05 15:34:37 -0500

    inital commit

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Level.score++;
+        Debug.Log(Level.score);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    public float JumpSpeed = 100.0f;
+    public float thrust = 1.0f;
+    private Rigidbody rb;
+
+    public bool setVelocity;
+    public Vector3 velocity;
+    public Vector3 currentVelocity;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (setVelocity)
+        {
+            setVelocity = false;
+            rb.velocity = velocity;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            Jump();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * thrust, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * thrust, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * thrust, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * thrust, ForceMode.Impulse);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, 1, 0) * JumpSpeed, ForceMode.Impulse);
+    }
+}
