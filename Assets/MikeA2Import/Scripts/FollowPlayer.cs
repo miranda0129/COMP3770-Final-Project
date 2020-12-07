b3f8865247f428e0adebb4f1c325e07a22fc7b6b
Michael Ward <mikeward312@gmail.com>
2020-12-06 21:41:40 -0500

    Import A2 stuff and messing with player script

---
+++ Assets/MikeA2Import/Scripts/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+     public GameObject player;
+     private float timer = 1f;
+     private float elapsedTime = 0f;
+     private Vector3 currPos;
+
+     void Start() {
+
+          currPos = player.transform.position;
+
+     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+          elapsedTime += Time.deltaTime;
+
+          if (player != null) {
+
+               Vector3 newPosition = player.transform.position;
+               newPosition.z -= 10.0f;
+               newPosition.y += 10.0f;
+
+               transform.position = newPosition;
+               transform.LookAt(player.transform.position);
+
+          } else {
+
+               player = GameObject.Find("Player");
+
+          }
+    }
+}
---
+++ Assets/MikeA2Import/Scripts/SafeZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col) {
+          
+          if(col.gameObject.name == "Player"){
+            // col.gameObject.GetComponent<MovePlayer>().isSafe = true;        // O.G
+            col.gameObject.GetComponent<Player>().isSafe = true;               // Playground
+               Debug.Log("Player is safe!");
+          }
+     }
+
+     void OnTriggerExit(Collider col) {
+          if(col.gameObject.name == "Player") {
+               
+           // col.gameObject.GetComponent<MovePlayer>().isSafe = false;         // O.G
+            col.gameObject.GetComponent<Player>().isSafe = false;               // Playground
+               Debug.Log("Player is no longer safe!");
+          }
+     }
+}
