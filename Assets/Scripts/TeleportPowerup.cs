94612d2e520cf22052e986e600395d6095a5b4f9
miranda0129 <56649574+miranda0129@users.noreply.github.com>
2020-12-08 16:15:15 -0500

    teleportation prefab created, also camera but while picking up laser is NOT fixed sry

--- Assets/Scripts/TeleportPowerup.cs
+++ Assets/Scripts/TeleportPowerup.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportPowerup : Powerup
+public class TeleportPowerup : MonoBehaviour
 {
     public static int count = 5;
+    public Vector3 clickPosition;
+    public AudioClip pop;
 
     void Start()
     {
+        pop = Resources.Load<AudioClip>("Audio Clips/teleport_pop");
     }
 
-    private void Update()
+    void Update()
     {
+        if (gameObject.name == "Player")
+        {
+            //telport onclick
+            if (Input.GetMouseButtonDown(0))
+            {
+                //play audio clip if available
+                if (pop != null) { AudioSource.PlayClipAtPoint(pop, transform.position); }
 
+                //get mouse position and set transform there
+                clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10)); //get click position
+                gameObject.GetComponent<Transform>().position = clickPosition;
+                count--;
+            }
+            if(count <= 0) {
+                Destroy(gameObject.GetComponent<TeleportPowerup>());
+                Debug.Log("Teleport power disabled, count 0");
+            }
+        }
     }
 }
