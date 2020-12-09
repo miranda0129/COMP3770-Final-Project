31ac7293d9132b26e47977b8c166e7d500694bce
Michael Ward <mikeward312@gmail.com>
2020-12-09 09:05:58 -0500

    camera fix

--- Assets/Scripts/SmoothFollowCam.cs
+++ Assets/Scripts/SmoothFollowCam.cs
@@ -6,11 +6,13 @@ public class SmoothFollowCam : MonoBehaviour {
     public Transform target;
     private Vector3 endPos;
     private Vector3 startPos;
-
-    public float smoothTime = 0.3f;
+    public float timebeforeUpdate = 0.3f;
+    private float smoothMultiplier = 1.0f;
+    private float smoothTime = 0.3f;
     private float elapsedTime = 0.0f;
     public float cameraHeight = 0;
 
+    public bool normalSpeed = true;
 
     void Start() {
         init();
@@ -28,12 +30,31 @@ public class SmoothFollowCam : MonoBehaviour {
 
         elapsedTime += Time.deltaTime;
 
-        transform.position = Vector3.Lerp(startPos, endPos, (elapsedTime / smoothTime));
+        smoothTime = elapsedTime / (timebeforeUpdate / smoothMultiplier);
+        transform.position = Vector3.Lerp(startPos, endPos, smoothTime);
 
+        // if we are close to the end position
         if(Vector3.Distance(transform.position, endPos) <= 0.1f || elapsedTime >= smoothTime) {
             startPos = transform.position;
-            Vector3 desiredPosition = target.TransformPoint(new Vector3(0, cameraHeight, -10));
-            endPos = new Vector3(Mathf.Clamp(desiredPosition.x, startPos.x, startPos.x + 10), desiredPosition.y, desiredPosition.z);
+            Vector3 desiredPosition = target.TransformPoint(new Vector3(0, cameraHeight, 0));
+            endPos = new Vector3(Mathf.Clamp(desiredPosition.x, startPos.x, Mathf.Infinity), desiredPosition.y, -10);
+
+
+            // Speed up camera at different thresholds.
+            float distance = Vector3.Distance(startPos, endPos);
+
+            if(!normalSpeed) {
+
+                if(distance <= 1.0f) {
+                    normalSpeed = true;
+                    NormalSpeed();
+                }
+
+            } else if(distance >= 5.0f) {
+                normalSpeed = false;
+                FastSpeed();
+            }
+
             elapsedTime = 0.0f;
         }
     }
@@ -45,4 +66,12 @@ public class SmoothFollowCam : MonoBehaviour {
         elapsedTime = 0;
 
     }
+
+    void NormalSpeed() {
+        smoothMultiplier = 1.0f;
+	}
+
+    void FastSpeed() {
+        smoothMultiplier = 2.0f;
+	}
 }
