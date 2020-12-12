f5f9e3644bac1c1a2ec9bbe64e1471eb22493d79
DiNardoR <46942999+DiNardoR@users.noreply.github.com>
2020-12-11 19:28:24 -0500

    Most features working
    Still have to fix the OnCollisionEnter issues I'm having with the spawned bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.collider.name);
+        print(collision.collider.tag);
+        print(collision.gameObject.tag);
+      /*  if (other.tag == "Player")
+        {
+            // damage player here
+            print("Hit Player");
+
+            Destroy(gameObject);
+        }*/
+    }
+
+
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,80 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-  public int damage;
-  
+    public int damage;
+    public int bulletSpeed;
+    public float aggroDistance;
+    public GameObject bullet;
+    public float gunCooldown;
+
+    private LineRenderer aimLine;
+    private bool reloading;
+
+    Vector3 playerPosition;
+
+    Rigidbody rb;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimLine = GetComponent<LineRenderer>();
+        aimLine.positionCount = 2;
+        reloading = false;
+
+    }
+    IEnumerator FireGun(Ray ray)
+    {
+        bullet = Instantiate(bullet, gameObject.transform.position, Quaternion.LookRotation(ray.direction));
+        //bullet.GetComponent<Rigidbody>().AddForce(ray.direction * bulletSpeed);
+        bullet.GetComponent<Rigidbody>().velocity = ray.direction * bulletSpeed;
+        reloading = true;
+        print("Started Reloading...");
+        yield return new WaitForSeconds(gunCooldown);
+
+        reloading = false;
+        print("Finished Reloading...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = GameObject.Find("Player").transform.position;
+
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(transform.position, playerPosition - transform.position); // Raycasts towards player position
+        aimLine.SetPosition(0, transform.position);
+
+        if (Physics.Raycast(ray, out hit, aggroDistance) && hit.collider.name == "Player") // If no obstacles between enemy and player, start shooting
+        {           
+            aimLine.SetPosition(1, playerPosition);
+
+            if (reloading == false)
+            {                           
+                StartCoroutine(FireGun(ray));
+            }
+                                             
+        }
+
+    }
+
+}
+
+/*
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damage;
+    public int bulletSpeed;
+    public float aggroDistance;
+    public LayerMask mask;
+
+    Vector3 playerPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +87,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerPosition = GameObject.Find("Player").transform.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, (playerPosition - transform.position), out hit, aggroDistance, mask))
+        {
+            print("I see player");
+            Debug.DrawRay(transform.position, (playerPosition - transform.position) * hit.distance, Color.red);
+        }
+
+        else
+        {
+            print("No player detected");
+        }
+            
+
 
     }
+
 }
+
+ */
