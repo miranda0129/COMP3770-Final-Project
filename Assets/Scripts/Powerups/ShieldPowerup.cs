be6011dd41fbbb27c2bd0ad0dbed1bca21198848
miranda0129 <56649574+miranda0129@users.noreply.github.com>
2020-12-14 17:27:42 -0500

    added sound for when shield blocks

--- Assets/Scripts/Powerups/ShieldPowerup.cs
+++ Assets/Scripts/Powerups/ShieldPowerup.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SheildPowerup : Powerup
 {
-
+    public AudioClip shieldBlock;
     // Start is called before the first frame update
     new void Start()
     {
@@ -13,6 +13,7 @@ public class SheildPowerup : Powerup
         //set player material and shield flag in player script
         player.SetMaterial(player.powerupMats[4]);
         player.isShielded = true;
+        shieldBlock = Resources.Load<AudioClip>("Audio Clips/shield"); //load audio clip
         Debug.Log("shield equipped");
     }
 
@@ -23,6 +24,7 @@ public class SheildPowerup : Powerup
         if (col.gameObject.layer == 11)
         {
             Debug.Log("Blocked by sheild");
+            if (shieldBlock != null) { AudioSource.PlayClipAtPoint(shieldBlock, transform.position); }
             player.isShielded = false;
             RemovePowerup(gameObject.GetComponent<SheildPowerup>());//destory after block
         }
@@ -30,6 +32,7 @@ public class SheildPowerup : Powerup
         else if (col.gameObject.name == "DamageHitbox")
         {
             Debug.Log("Blocked by sheild");
+            if (shieldBlock != null) { AudioSource.PlayClipAtPoint(shieldBlock, transform.position); }
             player.isShielded = false;
             RemovePowerup(gameObject.GetComponent<SheildPowerup>());//destory after block
         }
